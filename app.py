8c71fa98b1444f1eda39f757c5d5fa3849b599f5
Dragos <dragosgn@gmail.com>
2017-09-04 23:21:43 +0200

    added api

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ import connexion
 if __name__ == '__main__':
     app = connexion.App(__name__, 9090, specification_dir = 'swagger/')
     app.add_api('my_super_app.yaml', resolver=RestyResolver('api'))
-    
+    app.run()
